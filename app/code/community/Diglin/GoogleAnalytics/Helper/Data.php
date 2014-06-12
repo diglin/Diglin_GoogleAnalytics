ee99c645d40a0b1c547f1a35d3686bd9a7d954c0
Sylvain Rayé <sly@diglin.com>
2014-06-12 15:20:37 +0200

    helper changes

--- app/code/community/Diglin/GoogleAnalytics/Helper/Data.php
+++ app/code/community/Diglin/GoogleAnalytics/Helper/Data.php
@@ -1,10 +1,24 @@
 <?php
 /**
- * Diglin GmbH - Switzerland
+ * Diglin
  *
- * @category    brentford
- * @package     Diglin_
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to http://www.magentocommerce.com for more information.
+ *
+ * @category    Diglin
+ * @package     Diglin_GoogleAnalytics
  * @copyright   Copyright (c) 2011-2014 Diglin (http://www.diglin.com)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 class Diglin_GoogleAnalytics_Helper_Data extends Mage_Core_Helper_Data
 {
